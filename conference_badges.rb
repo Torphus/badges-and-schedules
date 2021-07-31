5bcc85378497938fdd3be2ed7f9117f7bc9cc572
Torrey Kanko <torphus101@gmail.com>
2021-07-31 10:30:16 -0700

    done

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list)
+    list.map do |name| 
+        badge_maker(name)
+    end
+end
+
+def assign_rooms(list)
+    list.map.with_index do |name, idx|
+        "Hello, #{name}! You'll be assigned to room #{idx + 1}!"
+    end
+end
+
+def printer(list)
+    batch_badge_creator(list).each do |msg|
+        puts msg
+    end
+    assign_rooms(list).each do |num|
+        puts num
+    end
+end
